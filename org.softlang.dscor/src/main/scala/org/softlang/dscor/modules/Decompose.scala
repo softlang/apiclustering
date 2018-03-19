883e632436f0350219a3b0ebea9eed0d532abed9
Johannes Haertel <johanneshaertel@uni-koblenz.de>
2018-03-19 17:33:03 +0100

    initial

--- org.softlang.dscor/src/main/scala/org/softlang/dscor/modules/Decompose.scala
+++
@@ -1,9 +0,0 @@
-package org.softlang.dscor.modules
-
-import org.softlang.dscor._
-import org.softlang.dscor.modules.access.Access
-import org.softlang.dscor.utils.InputStreamDelegate
-
-abstract class Decompose(
-    @Dependency var access: RDDModule[(String, InputStreamDelegate)]) extends RDDModule[(String, String)]  {
-}
